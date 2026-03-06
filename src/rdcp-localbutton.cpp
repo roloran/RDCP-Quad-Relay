8599b53960adb4d889a1cc7e31a26bf89fdfc819
Wolfgang Hommel <wolfgang.hommel@unibw.de>
2026-03-06 17:51:46 +0100

    Preliminary Local Button functionality implementation

--- src/rdcp-localbutton.cpp
+++ src/rdcp-localbutton.cpp
@@ -7,21 +7,25 @@
 bool local_button_initialized = false;
 uint64_t last_button_press = 0;
 
+#define LOCAL_BUTTON_PIN GPIO_NUM_35
+
 void local_button_check(void)
 {
+    int64_t now = my_millis();
+
     if (!local_button_initialized)
     {
-        // TODO 
+        pinMode(LOCAL_BUTTON_PIN, INPUT_PULLUP);
+        last_button_press = now;
         local_button_initialized = true;
     }
 
-    int64_t now = my_millis();
     if (now < last_button_press + 5 * SECONDS_TO_MILLISECONDS) return; // debounce
 
-    // TODO: Check for button press
-    if (false)
+    if (digitalRead(LOCAL_BUTTON_PIN) == LOW)
     {
         last_button_press = now;
+        serial_writeln("INFO: Local Button has been pressed!");
         serial_writeln("DA_LOCALBUTTON");
         rdcp_cmd_send_da_status_response(true);
     }
